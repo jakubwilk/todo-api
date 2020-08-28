cdb3e9b7acac2c55e9c742e46d8f94b57187a5ec
Jakub Wilk <jw@jakubwilk.pl>
2020-08-28 17:56:18 +0200

    Create role guard custom decorator

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
+import { RolesGuard } from './roles.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalGuards(new RolesGuard())
   await app.listen(process.env['APP_PORT']);
 }
 bootstrap();
